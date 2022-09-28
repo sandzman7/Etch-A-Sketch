9614eec85828ec7398844fb54890f28ea6568d21
sanders <sandzman7@gmail.com>
2022-09-28 15:41:37 -0400

    Finished

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+let color = "black";
+let click = false;
 document.addEventListener("DOMContentLoaded", function(){
-   createBox(32);
-   console.log("hi")
+   createBox(16);
+   
+document.querySelector("body").addEventListener("click", function(e){
+if(e.target.tagName != "BUTTON"){
+    click = !click;
+    let draw = document.querySelector("#draw");
+}
+else{
+    draw.innerHTML = "Draw!"
+}
 })
+
+   let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBox(size)
+    })
+
+})
+
+
    function createBox(size){
     let box = document.querySelector(".box");
     box.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -11,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+      div.addEventListener("mouseover", colorDiv)
         box.insertAdjacentElement("beforeend", div);
     }
 }
@@ -26,6 +46,24 @@ else if(input < 0 || input > 100){
 }
 else{
     message.innerHTML = "Go!"
+    return input;
 }
+}
+function colorDiv(){
+if(click){
+    if(color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+}
+else{
+    this.style.backgroundColor = 'black'
+}
+}
+}
+function setColor(colorChoice){
+ color = colorChoice;
+}
+function resetBox(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
    
